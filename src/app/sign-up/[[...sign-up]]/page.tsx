c74baa439bc6fff90c565d319699e7a693d3f6ca
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2024-11-12 01:03:12 +0530

    <auth> created sign up and sign in fallback routes

---
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div className="flex bg-gradient-to-tl from-primary-light via-secondary-dark to-primary-dark text-white items-center justify-around h-screen">
+      <SignUp />
+    </div>
+  );
+}
