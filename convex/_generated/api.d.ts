e61b877fae47a6a50cbbb9eb19bf9d9ff56a78f7
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2024-11-12 02:57:03 +0530

    <db> convex functions codegen

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -13,6 +13,12 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as addUserScore from "../addUserScore.js";
+import type * as createUser from "../createUser.js";
+import type * as deleteUser from "../deleteUser.js";
+import type * as getEverything from "../getEverything.js";
+import type * as getUserDocumentId from "../getUserDocumentId.js";
+import type * as getUserScores from "../getUserScores.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -22,7 +28,14 @@ import type {
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-declare const fullApi: ApiFromModules<{}>;
+declare const fullApi: ApiFromModules<{
+  addUserScore: typeof addUserScore;
+  createUser: typeof createUser;
+  deleteUser: typeof deleteUser;
+  getEverything: typeof getEverything;
+  getUserDocumentId: typeof getUserDocumentId;
+  getUserScores: typeof getUserScores;
+}>;
 export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
