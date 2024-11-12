087994954737b559af0fb1400f52c1cf1fcde7f1
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2024-11-13 05:17:06 +0530

    <contexts> using UserContext for auth status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import { UserContext } from "@/contexts/userContext";
 import { ClerkPortal } from "@/components/clerk-components/user-profile/user-profile";
+import Analytics from "@/components/custom-components/analytics/analytics";
 import { GamePanel } from "@/components/custom-components/game-panel/game-panel";
 
 export default function Home() {
+  const { user } = useUser();
+
   return (
     <>
       <ClerkPortal />
-      <section className="h-screen bg-gradient-to-tl from-primary-light via-secondary-dark to-primary-dark">
-        <GamePanel />
-      </section>
+      <UserContext.Provider value={user?.id || ""}>
+        <section className="h-screen bg-gradient-to-tl from-primary-light via-secondary-dark to-primary-dark">
+          <GamePanel />
+          <Analytics />
+        </section>
+      </UserContext.Provider>
     </>
   );
 }
