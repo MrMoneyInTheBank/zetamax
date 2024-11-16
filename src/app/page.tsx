634c86404fea359a288916ced847c894b9cc029e
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2024-11-17 05:15:28 +0530

    <contexts> wrapping app in scores context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,27 @@
 "use client";
 
 import { useUser } from "@clerk/nextjs";
+import { useLocalStorage } from "@/hooks/useLocalStorage";
 import { UserContext } from "@/contexts/userContext";
 import { ClerkPortal } from "@/components/clerk-components/user-profile/user-profile";
 import Analytics from "@/components/custom-components/analytics/analytics";
 import { GamePanel } from "@/components/custom-components/game-panel/game-panel";
+import { LocalScoresContext } from "@/contexts/localScoresContext";
 
 export default function Home() {
   const { user } = useUser();
+  const [localScores, setLocalScores] = useLocalStorage<number[]>("scores", []);
 
   return (
     <>
       <ClerkPortal />
       <UserContext.Provider value={user?.id || ""}>
-        <section className="h-screen bg-gradient-to-tl from-primary-light via-secondary-dark to-primary-dark">
-          <GamePanel />
-          <Analytics />
-        </section>
+        <LocalScoresContext.Provider value={{ localScores, setLocalScores }}>
+          <section className="h-screen bg-gradient-to-tl from-primary-light via-secondary-dark to-primary-dark">
+            <GamePanel />
+            <Analytics />
+          </section>
+        </LocalScoresContext.Provider>
       </UserContext.Provider>
     </>
   );
