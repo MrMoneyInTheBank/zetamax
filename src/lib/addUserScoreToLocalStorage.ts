29b8b440dc733c5dc2d54dfc28782a46afffe34a
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2024-11-17 05:35:48 +0530

    <lint> removed unused function

--- src/lib/addUserScoreToLocalStorage.ts
+++
@@ -1,8 +0,0 @@
-"use client";
-
-function addUserScoreToLS(score: number) {
-  const prevScores: number[] = JSON.parse(
-    localStorage.getItem("scores") ?? "[]",
-  );
-  localStorage.setItem("scores", JSON.stringify([...prevScores, score]));
-}
