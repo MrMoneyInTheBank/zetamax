bbda7a3189485261fe8c34f0c6aebc458a084385
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2024-12-23 23:01:59 +0530

    <test> useQuestion reset test

--- __tests__/hooks/useGameState.test.tsx
+++ __tests__/hooks/useGameState.test.tsx
@@ -24,4 +24,22 @@ describe("useGameState Hook", () => {
 
     expect(result.current.score).toBe(2);
   });
+
+  it("should reset the score to 0 when resetScore is called", () => {
+    const { result } = renderHook(() => useGameState());
+
+    act(() => {
+      result.current.incrementScore();
+      result.current.incrementScore();
+    });
+
+    expect(result.current.score).toBe(2);
+
+    act(() => {
+      result.current.resetScore();
+    });
+
+    expect(result.current.score).toBe(0);
+  });
+
 });
