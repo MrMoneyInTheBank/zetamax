5f7f3e3cd4721b6f498bbd4307cc87a205779590
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2025-04-29 02:17:42 +0530

    <ui> updating clerk loading phase login

--- src/components/clerk-components/user-profile/user-profile.tsx
+++ src/components/clerk-components/user-profile/user-profile.tsx
@@ -1,17 +1,29 @@
 "use client";
 
-import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { LogIn } from "lucide-react";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import { LoginButton } from "@/components/custom-components/login-button/login-button";
 
 export const ClerkPortal = () => {
   return (
     <div className="fixed right-4 top-4 z-30">
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <SignedOut>
-        <CustomSignInButton />
-      </SignedOut>
+      <ClerkLoading>
+        <LoginButton loading={true} />
+      </ClerkLoading>
+      <ClerkLoaded>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <CustomSignInButton />
+        </SignedOut>
+      </ClerkLoaded>
     </div>
   );
 };
@@ -19,11 +31,7 @@ export const ClerkPortal = () => {
 const CustomSignInButton = () => {
   return (
     <SignInButton mode="modal">
-      <div className="fixed top-4 right-4 flex items-center justify-center rounded-xl bg-gradient-to-br from-violet-600 to-violet-500">
-        <button className="relative rounded-xl bg-white/0 transition-colors hover:bg-white/20 p-1">
-          <LogIn color={"white"} size={45} />
-        </button>
-      </div>
+      <LoginButton />
     </SignInButton>
   );
 };
