c931ba379ed8f511cffc7e5161de31e95e476366
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2025-06-22 10:02:18 +0530

    <core> setting up http server

--- websocket-server/go.mod
+++ websocket-server/go.mod
@@ -5,4 +5,5 @@ go 1.24.4
 require (
 	github.com/gorilla/websocket v1.5.3
 	github.com/matoous/go-nanoid/v2 v2.1.0
+	github.com/rs/cors v1.11.1
 )
--- websocket-server/main.go
+++ websocket-server/main.go
@@ -1,3 +1,24 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/rs/cors"
+
+	"websocket-server/routes"
+)
+
 func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ws/health", routes.Health)
+	mux.HandleFunc("/ws/create-room", routes.CreateRoom)
+	mux.HandleFunc("/ws/delete-room", routes.DeleteRoom)
+	mux.HandleFunc("/ws/list-rooms", routes.ListRooms)
+
+	handler := cors.AllowAll().Handler(mux)
+
+	http.ListenAndServe(":8080", handler)
+
+	fmt.Println("Hello, World!")
 }
