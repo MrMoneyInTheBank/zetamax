43991d8d420e939969467f49a7ba745012dc0e8c
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2025-06-22 10:01:28 +0530

    <core> delete room api route

--- websocket-server/routes/routes.go
+++ websocket-server/routes/routes.go
@@ -32,3 +32,50 @@ func CreateRoom(w http.ResponseWriter, req *http.Request) {
 	w.Write(jsonResponse)
 }
 
+func DeleteRoom(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodDelete {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	} else if req.Header.Get("Content-Type") != "application/json" {
+		http.Error(w, "Content-Type must be application/json", http.StatusUnsupportedMediaType)
+		return
+	} else {
+		room.RoomsMutex.Lock()
+		defer room.RoomsMutex.Unlock()
+	}
+
+	var requestBody struct {
+		RoomID string `json:"roomID"`
+	}
+	if err := json.NewDecoder(req.Body).Decode(&requestBody); err != nil {
+		http.Error(w, "Failed to parse JSON body", http.StatusBadRequest)
+		return
+	}
+
+	roomID := requestBody.RoomID
+	if roomID == "" {
+		http.Error(w, "Missing room ID", http.StatusBadRequest)
+		return
+	}
+
+	if _, exists := room.Rooms[roomID]; !exists {
+		http.Error(w, "Room does not exist", http.StatusNotFound)
+		return
+	}
+
+	delete(room.Rooms, roomID)
+
+	w.WriteHeader(http.StatusOK)
+
+	response := map[string]string{
+		"message": "Deleted room",
+	}
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, "Something went wrong in json marshalling", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(jsonResponse)
+}
+
